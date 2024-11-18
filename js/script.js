c3d959f39f1ae5203c79366d8e661fb06fe88107
Tecipry <roald.gs@gmail.com>
2024-11-18 17:27:55 +0100

    url params

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// extract url parameters. Param definition is startet wirh ? and they are seperated with & in the url
+const url = document.URL;
+var params = url.split("?")[1].split("&");
+var paramPairs = params.reduce(function (acc, param) {
+    var [key, value] = param.split("=");
+    acc[key] = value;
+    return acc;
+}, {});
+
+// handle params:
+const parameters = Object.keys(paramPairs);
+
+// param pageId
+if (parameters.includes("pageId")) {
+    page = paramPairs["pageId"];
+    console.log(page);
+}
