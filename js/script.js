dce5c5c07eabc3ea655432c5c7106f656c064af0
Tecipry <roald.gs@gmail.com>
2024-11-18 19:55:49 +0100

    basic navigational layout

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // extract url parameters. Param definition is startet wirh ? and they are seperated with & in the url
 const url = document.URL;
-var params = url.split("?")[1].split("&");
-var paramPairs = params.reduce(function (acc, param) {
-    var [key, value] = param.split("=");
-    acc[key] = value;
-    return acc;
-}, {});
+var paramPairs = {};
+if (url.includes("?") && url.includes("=")) {
+   var params = url.split("?")[1].split("&");
+   paramPairs = params.reduce(function (acc, param) {
+      var [key, value] = param.split("=");
+      acc[key] = value;
+      return acc;
+   }, {});
+}
 
 // handle params:
 const parameters = Object.keys(paramPairs);
 
 // param pageId
 if (parameters.includes("pageId")) {
-    page = paramPairs["pageId"];
-    console.log(page);
+   page = paramPairs["pageId"];
+   console.log(page);
 }
