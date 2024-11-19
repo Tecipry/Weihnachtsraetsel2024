a65fd057c85141526f879cc2ebb7c03aca4cd47f
Tecipry <110675969+Tecipry@users.noreply.github.com>
2024-11-19 14:48:20 +0100

    Update riddleBoxRed.js

--- js/riddleBoxRed.js
+++ js/riddleBoxRed.js
@@ -1,16 +1,18 @@
 function create7x7Grid() {
-   console.log("create7x7Grid");
    // create a 7x7 grid of panels
    const panelGrid = document.getElementById('panel-grid');
+
    for (let i = 0; i < 7; i++) {
       let row = document.createElement('div');
-      row.style.display = 'flex';
+      row.className = 'gridRow7x7';
+
       for (let j = 0; j < 7; j++) {
          let panel = document.createElement('div');
          panel.className = 'panel';
          panel.style.backgroundColor = 'var(--seafoam_green)';
+
          panel.addEventListener('click', function() {
-            if (this.style.backgroundColor === 'var(--seafoam_green)') {
+            if (  this.style.backgroundColor === 'var(--seafoam_green)') {
                this.style.backgroundColor = 'var(--red)';
             } else {
                this.style.backgroundColor = 'var(--seafoam_green)';
@@ -20,4 +22,15 @@ function create7x7Grid() {
       }
       panelGrid.appendChild(row);
    }
+}
+
+function getCurrentGridValue() {
+   const panelGrid = document.getElementById('panel-grid');
+   const rows = panelGrid.children;
+   const panels = Array.from(rows).map(
+      row => Array.from(row.children).map(
+            panel => panel.style.backgroundColor === 'var(--seafoam_green)' ? 0 : 1
+         )
+      );
+   console.log(panels);
 }
