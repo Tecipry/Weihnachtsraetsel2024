d6bc2ecfff090161ca52cf0de7d3a2c016a0d837
Tecipry <roald.gs@gmail.com>
2024-11-20 22:39:19 +0100

    some html refactors and some snake game prototyping

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function loadNavBar() {
    const navBarDiv = document.getElementById("navBarDiv");
    navBarDiv.innerHTML = `
       <nav>
-         <ul style="list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: var(--hunter_green)">
+         <ul style="list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: var(--hunter_green); border: 4px solid var(--tea_green);">
             <li style="float: left">
                <a href="./overviewPage.html" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none">zurück zur Overview</a>
             </li>
@@ -10,19 +10,3 @@ function loadNavBar() {
       </nav>
    `;
 }
-
-// // cookies to store state of the riddle Boxes
-// riddleBoxesSolved = {
-//    "riddleBoxRed": false,
-//    "riddleBoxGreen": false,
-//    "riddleBoxOrange": false,
-//    "riddleBoxYellow": false
-// }
-
-// riddleBoxesStatus = getCookie("riddleBoxesStatus");
-// if (riddleBoxesStatus != null) {
-//    riddleBoxesSolved = JSON.parse(riddleBoxesStatus);
-// }
-// else {
-//    setCookie("riddleBoxesStatus", JSON.stringify(riddleBoxesSolved), 365);
-// }
