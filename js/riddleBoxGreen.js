72ad93d665680f986c1714628607243afe88df16
Tecipry <110675969+Tecipry@users.noreply.github.com>
2024-11-21 12:32:39 +0100

    snake game almost complete

--- js/riddleBoxGreen.js
+++ js/riddleBoxGreen.js
@@ -1,18 +1,31 @@
-
-var canvas = document.getElementById('snake_canvas');
-var context = canvas.getContext('2d');
-
-// the canvas width & height, snake x & y, and the apple x & y, all need to be a multiples of the grid size in order for collision detection to work
-// (e.g. 16 * 25 = 400)
+//// VARIABLES ////
 var gridSize = 16;
 var count = 0;
+var gameIsConfined = false;
 
+// set size of canvas to largest multiple of grid which is smaller than the viewport size
+const snakeGameContainer = document.getElementById('snakeGameContainer');
+const panel_canvas = document.getElementById('snake_panel_canvas');
+const snakeGameContainerWidth = document.getElementById('snakeGameContainer').offsetWidth;
+const snakeGameContainerHeight = document.getElementById('snakeGameContainer').offsetHeight;
+
+const panelCanvasWidth = Math.floor(snakeGameContainerWidth / gridSize) * gridSize + 'px';
+const panelCanvasHeight = Math.floor(snakeGameContainerHeight / gridSize) * gridSize + 'px';
+
+panel_canvas.style.width = panelCanvasWidth;
+panel_canvas.style.height = panelCanvasHeight;
+
+// amount of panels inside snakeGameContainer
+const amountOfPanelsHorizontal = Math.floor(snakeGameContainerWidth / gridSize);
+const amountOfPanelsVertical = Math.floor(snakeGameContainerHeight / gridSize);
+
+// snake Variables
 var snake = {
-   x: 160,
-   y: 160,
+   x: Math.floor(amountOfPanelsHorizontal / 2),
+   y: Math.floor(amountOfPanelsVertical / 2),
 
    // snake velocity. moves one grid length every frame in either the x or y direction
-   dx: gridSize,
+   dx: 1,
    dy: 0,
 
    // keep track of all grids the snake body occupies
@@ -21,21 +34,124 @@ var snake = {
    // length of the snake. grows when eating an apple
    maxCells: 4
 };
+//TODO: update this to fit dynamically
 var apple = {
-   x: 320,
-   y: 320
+   x: 20,
+   y: 20
 };
 
-var rusty_red = getComputedStyle(document.documentElement).getPropertyValue('--rusty_red');
-var hunter_green = getComputedStyle(document.documentElement).getPropertyValue('--hunter_green');
+
+//store all panels to manipulate them individually
+const indexForAllCanvasPanels = [];
+
+// fill panel_canvas with grid of divs (squares of size gridSize)
+for (let y = 0; y < amountOfPanelsVertical; y++) {
+   let row = document.createElement("div");
+   row.style.height = gridSize + 'px';
+   row.className = "panelCanvasRow";
+
+   const rowArray = [];
+
+   for (let x = 0; x < amountOfPanelsHorizontal; x++) {
+      let panel = document.createElement("div");
+      panel.className = "panelCanvasPanel";
+      panel.style.height = gridSize + 'px';
+      panel.style.width = gridSize + 'px';
+
+      rowArray.push(panel);
+      row.appendChild(panel);
+   }
+   indexForAllCanvasPanels.push(rowArray);
+   panel_canvas.appendChild(row);
+}
+
+function getIndividualPanel(x, y) {
+   if (x < 0 || x > indexForAllCanvasPanels[0].length || y < 0 || y > indexForAllCanvasPanels.length) {
+      return null;
+   }
+   return indexForAllCanvasPanels[y][x];
+}
+
+
+//// DEFINE BORDERS FOR GAME ////
+// border should be at 1/4 of the whole game area, both horizontally and vertically
+const upperBorderRow = Math.floor(amountOfPanelsVertical / 4);
+const lowerBorderRow = Math.floor(amountOfPanelsVertical * 3 / 4);
+const leftBorderColoumn = Math.floor(amountOfPanelsHorizontal / 4);
+const rightBorderColoumn = Math.floor(amountOfPanelsHorizontal * 3 / 4);
+
+//// create array of all border panels ////
+const cornerTopLext = [leftBorderColoumn, upperBorderRow];
+const cornerTopRight = [rightBorderColoumn, upperBorderRow];
+const cornerBottomLeft = [leftBorderColoumn, lowerBorderRow];
+const cornerBottomRight = [rightBorderColoumn, lowerBorderRow];
+
+var borderPanelCoordinates = [];
+// top row
+for (let x = leftBorderColoumn; x <= rightBorderColoumn; x++) {
+   borderPanelCoordinates.push([x, upperBorderRow]);
+}
+// bottom row
+for (let x = leftBorderColoumn; x <= rightBorderColoumn; x++) {
+   borderPanelCoordinates.push([x, lowerBorderRow]);
+}
+// left coloumn
+for (let y = upperBorderRow; y <= lowerBorderRow; y++) {
+   borderPanelCoordinates.push([leftBorderColoumn, y]);
+}
+// right coloumn
+for (let y = upperBorderRow; y <= lowerBorderRow; y++) {
+   borderPanelCoordinates.push([rightBorderColoumn, y]);
+}
+
+for (let i = 0; i < borderPanelCoordinates.length; i++) {
+   let workingPanel = getIndividualPanel(borderPanelCoordinates[i][0], borderPanelCoordinates[i][1]);
+   workingPanel.style.backgroundColor = "black";
+}
+
+
+//// MAIN SNAKE GAME ////
+
+//all panels which are coloured
+var allColouredPanels = [];
+
+//reset colour of snake panels
+function clearCanvas() {
+   for (let i = 0; i < allColouredPanels.length; i++) {
+      allColouredPanels[i].style.backgroundColor = "transparent";
+   }
+}
+
+//draw a panel in a colour
+function drawPanel(x, y, colour) {
+   let workingPanel = getIndividualPanel(x, y);
+   if (workingPanel === null) {
+      return;
+   }
+   workingPanel.style.backgroundColor = colour;
+   allColouredPanels.push(workingPanel);
+}
+
+//draw new apple
+//TODO: eliminate illegal apple spawn locations
+function newApple(confined) {
+   if (confined) {
+      console.log("spawning confined apple");
+      apple.x = getRandomInt(leftBorderColoumn + 1, rightBorderColoumn - 1);
+      apple.y = getRandomInt(upperBorderRow + 1, lowerBorderRow - 1);
+   } else {
+      console.log("spawning free apple");
+      apple.x = getRandomInt(0, amountOfPanelsHorizontal);
+      apple.y = getRandomInt(0, amountOfPanelsVertical);
+   }
+   drawPanel(apple.x, apple.y, 'var(--rusty_red)');
+}
 
 // get random whole numbers in a specific range
-// @see https://stackoverflow.com/a/1527820/2124254
 function getRandomInt(min, max) {
    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// game loop
 function loop() {
    requestAnimationFrame(loop);
 
@@ -45,27 +161,27 @@ function loop() {
    }
 
    count = 0;
-   context.clearRect(0,0,canvas.width,canvas.height);
+   clearCanvas()
 
    // move snake by it's velocity
    snake.x += snake.dx;
    snake.y += snake.dy;
 
-   // wrap snake position horizontally on edge of screen
-   if (snake.x < 0) {
-      snake.x = canvas.width - gridSize;
-   }
-   else if (snake.x >= canvas.width) {
-      snake.x = 0;
-   }
+   // // wrap snake position horizontally on edge of screen
+   // if (snake.x < 0) {
+   //    snake.x = canvas.width - gridSize;
+   // }
+   // else if (snake.x >= canvas.width) {
+   //    snake.x = 0;
+   // }
 
-   // wrap snake position vertically on edge of screen
-   if (snake.y < 0) {
-      snake.y = canvas.height - gridSize;
-   }
-   else if (snake.y >= canvas.height) {
-      snake.y = 0;
-   }
+   // // wrap snake position vertically on edge of screen
+   // if (snake.y < 0) {
+   //    snake.y = canvas.height - gridSize;
+   // }
+   // else if (snake.y >= canvas.height) {
+   //    snake.y = 0;
+   // }
 
    // keep track of where snake has been. front of the array is always the head
    snake.cells.unshift({x: snake.x, y: snake.y});
@@ -76,40 +192,35 @@ function loop() {
    }
 
    // draw apple
-   context.fillStyle = rusty_red;
-   context.fillRect(apple.x, apple.y, gridSize-1, gridSize-1);
+   drawPanel(apple.x, apple.y, 'var(--rusty_red)');
 
    // draw snake one cell at a time
-   context.fillStyle = hunter_green;
    snake.cells.forEach(function(cell, index) {
 
-      // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
-      context.fillRect(cell.x, cell.y, gridSize-1, gridSize-1);
+      drawPanel(cell.x, cell.y, 'var(--hunter_green)');
 
       // snake ate apple
       if (cell.x === apple.x && cell.y === apple.y) {
-      snake.maxCells++;
+         snake.maxCells++;
 
-      // canvas is 400x400 which is 25x25 grids
-      apple.x = getRandomInt(0, 25) * gridSize;
-      apple.y = getRandomInt(0, 25) * gridSize;
+         newApple(gameIsConfined);
       }
 
       // check collision with all cells after this one (modified bubble sort)
       for (var i = index + 1; i < snake.cells.length; i++) {
 
-      // snake occupies same space as a body part. reset game
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-         snake.x = 160;
-         snake.y = 160;
-         snake.cells = [];
-         snake.maxCells = 4;
-         snake.dx = gridSize;
-         snake.dy = 0;
+         // snake occupies same space as a body part. reset game
+         // TODO: update death logic (death on border, death reveals text)
+         if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+            snake.x = 25;
+            snake.y = 25;
+            snake.cells = [];
+            snake.maxCells = 4;
+            snake.dx = 1;
+            snake.dy = 0;
 
-         apple.x = getRandomInt(0, 25) * gridSize;
-         apple.y = getRandomInt(0, 25) * gridSize;
-      }
+            newApple(gameIsConfined);
+         }
       }
    });
 }
@@ -123,55 +234,29 @@ document.addEventListener('keydown', function(e) {
 
    // left arrow key
    if (e.which === 37 && snake.dx === 0) {
-      snake.dx = -gridSize;
+      snake.dx = -1;
       snake.dy = 0;
    }
    // up arrow key
    else if (e.which === 38 && snake.dy === 0) {
-      snake.dy = -gridSize;
+      snake.dy = -1;
       snake.dx = 0;
    }
    // right arrow key
    else if (e.which === 39 && snake.dx === 0) {
-      snake.dx = gridSize;
+      snake.dx = 1;
       snake.dy = 0;
    }
    // down arrow key
    else if (e.which === 40 && snake.dy === 0) {
-      snake.dy = gridSize;
+      snake.dy = 1;
       snake.dx = 0;
    }
 });
 
-// start the game
-// requestAnimationFrame(loop);
 
-
-//// EXPORTED FUNCTIONS for riddleBoxGreen.html ////
-var gameRunning = false;
-function startSnakeGame() {
-   if (gameRunning) {
-      return;
-   }
+function startGame () {
    requestAnimationFrame(loop);
-   gameRunning = true;
 }
-window.startSnakeGame = startSnakeGame;
-
-// set size of canvas to largest multiple of grid which is smaller than the viewport size
-var snakeGameContainer = document.getElementById('snakeGameContainer');
-var canvas = document.getElementById('snake_canvas');
-var snakeGameContainerWidth = document.getElementById('snakeGameContainer').offsetWidth;
-var snakeGameContainerHeight = document.getElementById('snakeGameContainer').offsetHeight;
-
-canvas.style.width = Math.floor(snakeGameContainerWidth / gridSize) * gridSize + 'px';
-canvas.style.height = Math.floor(snakeGameContainerHeight / gridSize) * gridSize + 'px';
-
-
-//set size of snakeGameContainer to largest multiple of grid which is smaller than the viewport size
-// var snakeGameContainer = document.getElementById('snakeGameContainer');
-// var snakeGameContainerWidth = document.getElementById('navBarDiv').offsetWidth;
-// var snakeGameContainerHeight = window.innerHeight - (document.getElementById('navBarDiv').offsetHeight);
-
-// snakeGameContainer.style.width = Math.floor(snakeGameContainerWidth / gridSize) * gridSize + 'px';
-// snakeGameContainer.style.height = Math.floor(snakeGameContainerHeight / gridSize) * gridSize + 'px';
+window.startGame = startGame;
+// TODO: start game on first key press
